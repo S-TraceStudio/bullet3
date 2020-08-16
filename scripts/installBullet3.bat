6b1579cdd60eefba48d3bfd9a0858d58dcb5ef2d
Franck Chateau <franck@s-trace.com>
2020-08-16 05:35:34 +0200

    direct xcopy command to install folder

--- scripts/installBullet3.bat
+++ scripts/installBullet3.bat
@@ -20,7 +20,26 @@ Set CMAKE_INSTALL_RELEASE= -DBUILD_TYPE=Release
 cmake %CMAKE_LOCAL_ONLY% %CMAKE_INSTALL_DEBUG% %CMAKE_INSTALL_DEBUG_PATH% %CMAKE_INSTALL%
 cmake %CMAKE_LOCAL_ONLY% %CMAKE_INSTALL_RELEASE% %CMAKE_INSTALL_RELEASE_PATH% %CMAKE_INSTALL%
 
+cd ..
+
+:: copy library files
+xcopy "./build/lib/release" "./install/bullet3/x64/Release/lib" /s /y /i
+xcopy "./build/lib/debug" "./install/bullet3/x64/Debug/lib" /s /y /i
+
+:: copy header files
+xcopy "./src" "./install/bullet3/x64/Release/include" /s /y /i
+xcopy "./src" "./install/bullet3/x64/Debug/include" /s /y /i
+
+cd install
+
+:: delete files
+del /s /q *.cpp
+del /s /q *.txt
+del /s /q *.lua
+del /s /q *.c
+
+:: move back
+cd ..
 
 :: move to another directory
-cd ..
 cd scripts
