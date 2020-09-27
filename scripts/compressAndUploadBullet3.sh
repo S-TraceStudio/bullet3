978d467fbdce9f6b4233fe541e355a7c3e6c173f
Franck Chateau <franck@s-trace.com>
2020-09-27 20:01:24 +0200

    updating filename

--- scripts/compressAndUploadBullet3.sh
+++ scripts/compressAndUploadBullet3.sh
@@ -3,16 +3,16 @@ cd ..
 cd install
 
 #delete old file
-rm Bullet3-OSX.zip
+rm bullet3-OSX.zip
 
 #compress folder
-zip -r -X Bullet3-OSX.zip ./Bullet3
+zip -r -X bullet3-OSX.zip ./bullet3
 
 #upload to s3 bucker
-aws s3 cp ./Bullet3-OSX.zip s3://s-trace.dependencies/
+aws s3 cp ./bullet3-OSX.zip s3://s-trace.dependencies/
 
 #give public READ permission
-aws --endpoint-url https://s3-ap-southeast-1.amazonaws.com/ s3api put-object-acl --bucket s-trace.dependencies --key Bullet3-OSX.zip --acl public-read
+aws --endpoint-url https://s3-ap-southeast-1.amazonaws.com/ s3api put-object-acl --bucket s-trace.dependencies --key bullet3-OSX.zip --acl public-read
 
 #
 cd ..
